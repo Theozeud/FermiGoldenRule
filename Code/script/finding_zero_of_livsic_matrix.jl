78e1a60a37f6be29dcdef5a469c45a335b299a86
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2024-06-02 11:43:11 +0200

    update

---
+++ Code/script/finding_zero_of_livsic_matrix.jl
@@ -0,0 +1,50 @@
+using Plots             # For plotting results
+using LaTeXStrings      # For the utilisation of Latex in Plots
+using LinearAlgebra     # For matrix computation
+using Optim             # For optimization function
+using Distances         # For Distance function
+
+using FermiGoldenRule
+
+# Modelisation of the Hamiltonian
+A(N::Int) = SymTridiagonal(zeros(N), ones(N-1))
+
+δ(R::Int, Nmin::Int, Nmax::Int) = [R==i ? 1 : 0 for i in Nmin:Nmax] #Nmin:Nmax
+
+h(E::Real, ϵ::Real, R₀::Int, Nmax::Int, Nmin::Int) = (z::Number, ϕ::Vector) -> (E*z + ϵ * ϕ[-Nmin+1+R₀], ϵ*z*δ(R₀,Nmin, Nmax)+A(length(ϕ))*ϕ)
+
+# Initial condition
+z₀ = 1.0
+ϕ₀fun = (z₀, (N::Int) -> 0)
+
+# Choice of model parameters
+const R₀ = 0
+E = 0.2
+ϵ = 0.5
+
+# Parameters for the approximation
+Nmax = 25                   # Upper bound of the lattice
+Nmin = - Nmax               # Lower bound of the lattice    
+Lattice = Nmin:Nmax         # Definition of the discrete lattice
+Nₜ = 2500                    # Definition of the number of timestep
+T = 25                      # Duration of the simulation
+timeT = LinRange(0,T,Nₜ+1)   # Time Lattice
+
+# Initial condition on the lattiche
+ϕ₀ = [ϕ₀fun[1], ϕ₀fun[2].(Lattice)...]
+
+# Computation of the dynamics
+sol = dynamics(h(E, ϵ, R₀, Nmax, Nmin), ϕ₀fun, (C,L2Z(Nmin,Nmax)); T = T, Nₜ = Nₜ)
+
+# To find the coefficient Γ(ϵ), we minimze the function equal to the solution minus exp(-2Γ(ϵ)tϵ^2)
+
+# Loss function
+loss(Γ, sol, time) = norm(sqeuclidean(proba(ϕ₀,sol),exp.(-Γ[1].*time*ϵ^2)))^2
+
+# Init guess for the optimization
+Γ₀ = 1.0
+
+opt = optimize(Γ -> loss(Γ, sol, timeT), [Γ₀]; iterations=100)
+res = Optim.minimizer(opt)
+
+
